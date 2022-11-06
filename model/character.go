cfe7d8bf7cd882694e1a488bd686867f124f3f22
supernova0730 <kuanysheveldar123@gmail.com>
2022-11-06 17:31:11 +0600

    remove gorm tags from model

--- model/character.go
+++ model/character.go
@@ -1,13 +1,13 @@
 package model
 
 type Character struct {
-	ID      int64  `gorm:"column:id"`
-	Name    string `gorm:"column:name"`
-	Status  string `gorm:"column:status"`
-	Species string `gorm:"column:species"`
-	Type    string `gorm:"column:type"`
-	Gender  string `gorm:"column:gender"`
-	Image   string `gorm:"column:image"`
+	ID      int64
+	Name    string
+	Status  string
+	Species string
+	Type    string
+	Gender  string
+	Image   string
 }
 
 func (Character) TableName() string {
