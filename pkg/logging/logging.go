b8338093a0b80ac0f874ff0bbd7cfa6d8940a11b
yekuanyshev <kuanysheveldar123@gmail.com>
2023-04-14 16:23:21 +0600

    added logging package

---
+++ pkg/logging/logging.go
@@ -0,0 +1,23 @@
+package logging
+
+import "log"
+
+func Fatal(message string) {
+	log.Fatalf("[FATAL] %s\n", message)
+}
+
+func Error(message string) {
+	log.Println("[ERROR]", message)
+}
+
+func Debug(message string) {
+	log.Println("[DEBUG]", message)
+}
+
+func Warn(message string) {
+	log.Println("[WARN]", message)
+}
+
+func Info(message string) {
+	log.Println("[INFO]", message)
+}
